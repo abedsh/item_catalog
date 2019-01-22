77120fcbfba816430a693803427b65d9b8511b2f
Ubuntu <ubuntu@ip-172-26-8-164.us-east-2.compute.internal>
2019-01-22 21:41:21 +0000

    updates

---
+++ __init__.py
@@ -0,0 +1,18 @@
+from app import app
+
+if __name__ == '__main__':
+    app.secret_key = 'super secret key'
+#    app.debug = True
+    app.run()
+
+
+
+
+
+#from flask import Flask
+#app = Flask(__name__)
+#@app.route("/")
+#def hello():
+#    return "Hello, I love Digital Ocean!"
+#if __name__ == "__main__":
+#    app.run()
--- run.py
+++
@@ -1,4 +0,0 @@
-from app import app
-
-app.secret_key = 'super secret key'
-app.run()
